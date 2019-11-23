c4f7aa3f6a896cb79b5b23baae9490146ad23341
veeshanoo <visanu.cristian@gmail.com>
2019-11-23 23:57:15 +0200

    updated main.go

--- main.go
+++ main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"banking/mongodb"
 	"banking/server"
-	"fmt"
-	"go/build"
 	"os"
 )
 
 func main() {
 	mongodb.Init()
-	gopath := os.Getenv("GOPATH")
-	if gopath == "" {
-		fmt.Println("bad")
-		gopath = build.Default.GOPATH
-	}
-	fmt.Println(gopath)
 	port := os.Args[1]
 	server.RunServer(port)
 }
